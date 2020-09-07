f7b57c5603026966076895e069ee8e90874ea713
unknown <calebhay@rocketmail.com>
2020-09-07 13:45:47 -0700

    Search and Save pages set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 import { Container } from 'react-bootstrap';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 
 function App() {
   return (
@@ -10,8 +13,12 @@ function App() {
       <NavBar />
       <Container>
         <Header />
-
         
+        <Router>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
+        </Router>
       </Container>
     </div>
   );
---
+++ client/src/pages/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchForm from "../components/SearchForm";
+import Results from "../components/Results";
+
+function Search() {
+    return (
+        <div>
+            <SearchForm />
+            <Results />
+        </div>
+    );
+}
+
+export default Search;
