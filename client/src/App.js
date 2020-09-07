3bcdee8eef918754655a2cc86f48e8b63b772d05
unknown <calebhay@rocketmail.com>
2020-09-07 14:21:15 -0700

    Added useEffect for Google Books API

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import { Container } from 'react-bootstrap';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
@@ -6,14 +6,24 @@ import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Search from './pages/Search';
 import Saved from './pages/Saved';
+import googleBooksAPI from "./utils/googleBooksAPI";
 
 function App() {
+
+  useEffect(() => {
+    googleBooksAPI
+      .searchBooks("Douglas Adams")
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => console.log("Error", err));
+  }, []);
+
   return (
     <div>
       <NavBar />
       <Container>
         <Header />
-        
         <Router>
           <Route exact path="/" component={Search} />
           <Route exact path="/search" component={Search} />
